290f4b7673c686e4dc121ceb7c04cf3450815ec3
Yves Ouvrard, alias Collatinus <yves.ouvrard@collatinus.org>
2016-03-03 09:49:17 +0100

    correction des classes syntaxe

--- src/syntaxe.h
+++ src/syntaxe.h
@@ -3,11 +3,24 @@
 /*
    Module d'analyse syntaxique.
 
+Une règle a deux membres : père et fils
+Une expression peut avoir trois membres ou plus. Exemple :
+nominatif + datif + sum
+On résout en posant une règle supplémentaire
 
+sumDatif:
+- fils datif
+- sum père d'un sujet au nominatif.
 
+Algo : toujours envisager le mot cliqué comme sub. Recherche
+vers la gauche, puis vers la droite.
 
-
-
+- si on rencontre le super immédiatement à gauche ou à droite,
+  on peut renvoyer le résultat.
+- si l'élément n'est pas le super, on passe la main à cet 
+  élément, qui doit chercher un père à son tour.
+- si le père est trouvé, on considère l'élément étranger comme
+  résolu, et on reprend la recherche précédente.
 
 */
 
@@ -16,19 +29,30 @@
 
 #include "lemmatiseur.h"
 
+class ElS: public QObject
+{
+	Q_OBJECT
+
+	private:
+		QStringList    _lemmes;
+		QStringList    _pos;
+		QStringList    _morphos;
+};
+
 class RegleS: public QObject
 {
 	Q_OBJECT
 
 	private:
-		QString     _id;
-		QStringList _lemmesF;
-		QStringList _lemmesP;
-		QStringList _morphosF;
-		QStringList _morphosP;
-		QString     _traduction;
+		QString        _accord;
+		QString        _id;
+		RegleS        *_pere;
+		QString        _sens;
+		ElS           *_super;
+		ElS           *_sub;
+		QString        _traduction;
 	public:
-		RegleS (QString lin);
+		RegleS (QStringList lignes);
 };
 
 class Syntaxe: public QObject
@@ -37,7 +61,9 @@ class Syntaxe: public QObject
 	Q_OBJECT
 
 	private:
-		QList<RegleS*> regles;
+		QList<RegleS*> _regles;
+	public:
+		Syntaxe ();
 };
 
 #endif
