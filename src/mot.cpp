557dfd841f54b82978dc5edea68288adf658efc9
PhVerkerk <Philippe.Verkerk@univ-lille1.fr>
2016-11-09 09:10:12 +0100

    Pronoms contractés -cum

--- src/mot.cpp
+++ src/mot.cpp
@@ -2,6 +2,7 @@
 
 Mot::Mot(QString forme, int rang, QObject *parent)
 {
+//    qDebug() << forme;
     _forme = forme;
     _rang = rang;
     _lemmatiseur = qobject_cast<Lemmat *>(parent);
@@ -47,6 +48,11 @@ Mot::Mot(QString forme, int rang, QObject *parent)
         // J'ai aussi une QMap qui associe les tags aux probas, que je dois normaliser.
         long total = 0;
         foreach (QString t, _probas.keys()) total += _probas[t];
+        if (total == 0)
+        {
+            total = 1;
+            qDebug() << forme << " : toutes les probas sont nulles !";
+        }
         foreach (QString t, _probas.keys())
         {
             long pr = _probas[t] * 1024 /total;
@@ -59,11 +65,35 @@ Mot::Mot(QString forme, int rang, QObject *parent)
         else if (enclitique == "st") _tagEncl = "v11";
         if (forme.endsWith("cum"))
         {
-            bool encl = (forme == "mecum") || (forme == "tecum") || (forme == "secum") || (forme == "quicum");
-            encl = encl || (forme == "mecum") || (forme == "nobiscum") || (forme == "vobiscum") || (forme == "quibuscum");
-            if (encl) _tagEncl = "re ";
+            bool encl = (forme == "mecum") || (forme == "tecum") || (forme == "secum");
+            encl = encl || (forme == "nobiscum") || (forme == "vobiscum") || (forme == "quibuscum");
+            encl = encl || (forme == "quacum") || (forme == "quocum") || (forme == "quicum");
+            if (encl)
+            {
+//                qDebug() << forme << " avec enclitique";
+                _tagEncl = "re ";
+                if (_tags.isEmpty())
+                {
+                    _tags.append("p61");
+                    _probas.insert("p61",1024);
+                    _lemmes.append(forme);
+                    _morphos.append("...");
+                    _nbOcc.append(1);
+                }
+                else if (_probas.size() == 1)
+                {
+                    if (!_probas.keys().contains("p61"))
+                    {
+                        _tags[0]="p61";
+                        _probas.clear();
+                        _probas.insert("p61",1024);
+                    }
+                }
+                else qDebug() << "Erreur sur " << forme << " : " << _tags;
+            }
         }
     }
+//    qDebug() << forme;
 }
 
 QString Mot::choisir(QString t, bool tout)
