7bb6d247bbdd7aac1bab5fa6467e6b741e87c030
PhVerkerk <Philippe.Verkerk@univ-lille1.fr>
2017-12-11 17:42:55 +0100

    correction de bugs dans la recherche d'hexamètres dans la prose

--- src/scandeur.cpp
+++ src/scandeur.cpp
@@ -21,6 +21,57 @@
 
 #include "scandeur.h"
 
+/**
+ * @brief Scandeur::Scandeur
+ * \if French
+ * Créateur de la classe pour scander des textes.
+ * \else
+ * Constructs a Scandeur to scan the texts.
+ * \endif
+ * @param parent :
+ * \if French
+ * Un pointeur vers l'objet qui crée cette classe.
+ * \else
+ * The owner of this class.
+ * \endif
+ * @param l :
+ * \if French
+ * Un pointeur vers un moteur de lemmatisation (LemCore).
+ * \else
+ * A pointeur to the lemmatization core (LemCore).
+ * \endif
+ * @param resDir :
+ * \if French
+ * Le chemin complet du dossier contenant les fichiers de donnée.
+ * \else
+ * The absolute path for the resources directory.
+ * \endif
+ *
+ * \if French
+ * La classe Scandeur propose les outils nécessaires pour scander un texte.
+ * Elle utilise le moteur de lemmatisation de Collatinus (LemCore)
+ * qui lui est passé en paramètre. Si ce moteur ne lui est pas donné,
+ * elle le crée ici.
+ * Si l'application envisagée utilise plusieurs modules
+ * intermédiaires (tagueur, lemmatiseur...),
+ * il vaut mieux créer un seul moteur commun.
+ *
+ * Le paramètre optionnel resDir donne
+ * le chemin complet du dossier contenant les fichiers de donnée.
+ * Par défaut, il s'agit du fichier "data" placé à côté de l'exécutable.
+ * \else
+ * The Scandeur class is meant to scan texts.
+ * As part of Collatinus, it uses its lemmatization core (LemCore).
+ * If this core has been created elsewhere, a pointer is given to this creator.
+ * If the pointer l is empty, then the lemmatization core is created here.
+ * When the developped application uses different modules as this one
+ * (e.g. Lemmatiseur, Tagueur), it is recommended to create a shared core.
+ *
+ * The optional parameter resDir gives the absolute path to the
+ * resources directory. If empty, the resources are assumed to be
+ * in a folder "data" placed in the same dir as the exe.
+ * \endif
+ */
 Scandeur::Scandeur(QObject *parent, LemCore *l, QString resDir) : QObject(parent)
 {
     if (l==0)
@@ -41,9 +92,27 @@ Scandeur::Scandeur(QObject *parent, LemCore *l, QString resDir) : QObject(parent
 
 
 /**
- * \fn void Scandeur::lisParPos()
- * \brief Lecture des règles de quantité par position
+ * \brief
+ * \if French
+ * Lecture des règles de quantité par position
+ * \else
+ * to read the rules for the quantities by position
+ * \endif
+ *
+ * \if French
+ * Les règles de quantité par position sont
  * enregistrées dans le fichier data/parpos.txt.
+ * Elles se présentent comme une expression rationnelle,
+ * suivie par la chaine de remplacement.
+ * Elles sont utilisées dans la fonction parPos.
+ * \else
+ * The rules to determine the quantities by position
+ * are stored in the file "data/parpos.txt".
+ * They take the form of a regular expression followed
+ * by the remplacement string.
+ * They are used in the parPos function.
+ * \endif
+ *
  */
 void Scandeur::lisParPos()
 {
@@ -58,13 +127,38 @@ void Scandeur::lisParPos()
 
 /**
  * @brief Scandeur::parPos
- * @param f : la forme sans quantité
- * @return : la forme avec les quantités déterminables par position.
+ * \if French
+ * détermine les quantités par position.
+ * \else
+ * determines the quantities by position.
+ * \endif
+ * @param f :
+ * \if French
+ * la forme sans quantité
+ * \else
+ * the form without quantity
+ * \endif
+ * @return
+ * \if French
+ * la forme avec les quantités déterminables par position.
+ * \else
+ * the form with all the quantities that can be determined bytheir position
+ * \endif
  *
+ * \if French
  * Quand une forme n'est pas reconnue,
  * on essaie quand même de trouver les quantités.
  * Ça n'est possible que pour les quantités définies par position.
  * On ne peut pas deviner les quantités par nature.
+ * Les règles utilisées sont données dans le fichier parpos.txt
+ * qui est lu par la routine lisParPos.
+ * \else
+ * When a form is not known, some of its quantities can
+ * be deduced from their position.
+ * One cannot know the quantities by nature.
+ * The rules are given in the file parpos.txt
+ * which is read in the lisParPos function.
+ * \endif
  */
 QString Scandeur::parPos(QString f)
 {
@@ -77,15 +171,45 @@ QString Scandeur::parPos(QString f)
 }
 
 /**
- * \fn QStringList Scandeur::cherchePieds (int nbr, QString ligne, int i, bool
- *pentam)
- * \brief cherchePieds est une routine récursive qui construit la liste
- * de toutes les combinaisons possibles de dactyles et de spondées
- * en commençant par le caractère d'indice i.
- * Elle est récursive car chaque fois qu'elle a trouvé un D ou un S,
- * elle va chercher toutes les combinaisons possibles à partir
- * du caractère d'indice i+3 ou i+2.
- * Les longues sont notées + et les brèves -.
+ * @brief Scandeur::cherchePieds
+ * \if French
+ * recherche de dactyles et de spondées dans le schéma métrique d'un vers.
+ * \else
+ * \endif
+ * @param nbr :
+ * \if French
+ * nombre de mètres recherchés.
+ * \else
+ * \endif
+ * @param ligne :
+ * \if French
+ * Le schéma métrique du vers.
+ * \else
+ * \endif
+ * @param i :
+ * \if French
+ * Point de départ en nombre de caractère.
+ * \else
+ * \endif
+ * @param pentam :
+ * \if French
+ * Pour la recherche de pentamètres.
+ * \else
+ * \endif
+ * @return
+ * \if French
+ * La liste de toutes les combinaisons de dactyles et spondées trouvées.
+ * \else
+ * \endif
+ *
+ * \if French
+ * Le but est de déceler d'éventuels
+ * hexamètres ou pentamètres dans de la prose.
+ * Si la fin de vers n'est pas répérée, on ne peut pas appliquer
+ * la méthode habituelle de scansion qui consiste à commencer par la fin.
+ *
+ * Le schéma métrique ne contient que les longueurs des syllabes.
+ * Ici, les longues sont notées + et les brèves -.
  * Les mots sont séparés par un espace et on a gardé une trace
  * des élisions avec un `.
  * Une grande partie de la difficulté vient des voyelles communes
@@ -94,7 +218,31 @@ QString Scandeur::parPos(QString f)
  * toutes les possibilités :
  * s=*+, +* ou **
  * d=*--, +*-, +-*, +**, *-*, **- ou ***.
- **************************/
+ * Le découpage en mètres n'est plus nécessairement univoque.
+ *
+ * cherchePieds est une routine récursive qui construit la liste
+ * de toutes les combinaisons possibles de dactyles et de spondées
+ * en commençant par le caractère d'indice i.
+ * Elle est récursive car chaque fois qu'elle a trouvé un D (ou d) ou un S (ou s),
+ * elle va chercher toutes les combinaisons possibles à partir
+ * du caractère d'indice i+3 ou i+2.
+ *
+ * Un hexamètre sera représenté par un séquence de cinq mètres,
+ * dactyles (D ou d) ou spondées (S ou s) suivi d'un sixième noté X
+ * (presque spondée: deux syllabes dont la première est longue,
+ * la dernière étant indifférente).
+ * Le pentamètre est lui composé de deux groupes de deux mètres,
+ * dactyles ou spondées, séparés par une longue (notée Y ou y)
+ * et terminé par une syllabe indifférente (notée Z).
+ *
+ * Le paramètre pentam est false au début de la recherche, qui va donc
+ * chercher indifféremment pentamètres et hexamètres.
+ * Après avoir trouvé deux mètres et une longue supposés former le début
+ * d'un pentamètre, il ne faut plus que chercher une fin de pentamètre :
+ * pour cela, pentam bascule à true.
+ * \else
+ * \endif
+ */
 QStringList Scandeur::cherchePieds(int nbr, QString ligne, int i, bool pentam)
 {
     QStringList res;
@@ -201,11 +349,15 @@ QStringList Scandeur::cherchePieds(int nbr, QString ligne, int i, bool pentam)
  * @param debPhr : booléen qui indique que l'initiale peut être en majuscule.
  * @param accent : entier qui détermine le comportement de l'accentuation.
  * @return Renvoie une liste avec la forme scandée (ou accentuée) de
- * toutes les manières possibles en appliquant
- * les quantités données par les dictionnaires et les règles prosodiques.
+ * toutes les manières possibles.
  *
  * Cette fonction commence par lemmatiser la forme donnée
  * en utilisant le lemmatiseur de Collatinus.
+ * Elle renvoie alors une liste avec la forme scandée (ou accentuée) de
+ * toutes les manières possibles en appliquant
+ * les quantités données par les dictionnaires et les règles prosodiques.
+ * Si le mot n'est pas reconnu, seules les quantités déterminables
+ * par leur position sont indiquées.
  * Les solutions sont ordonnées en fonction de leur fréquence,
  * la plus probable venant en premier.
  *
@@ -284,15 +436,44 @@ QStringList Scandeur::formeq(QString forme, bool *nonTrouve, bool debPhr,
 
 /**
  * @brief Scandeur::scandeTxt
- * Scande le texte, avec les statistiques si stats
- *        est à true, et renvoie le résultat.
- * @param texte : le texte à scander ou à accentuer
- * @param accent : un entier qui détermine si le résultat est
+ * \if French
+ * Scande ou accentue le texte.
+ * \else
+ * Scan the text.
+ * \endif
+ * @param texte :
+ * \if French
+ * le texte à scander ou à accentuer
+ * \else
+ * the text to be scanned or accented.
+ * \endif
+ * @param accent :
+ * \if French
+ * un entier qui détermine si le résultat est
  * scandé ou accentué.
- * @param stats : booléen qui affiche ou non les statistiques
- * @param majAut : booléen qui autorise les majuscules initiales
+ * \else
+ * an integer that determine the behavior
+ * \endif
+ * @param stats :
+ * \if French
+ * booléen qui affiche ou non les statistiques
+ * \else
+ * to compute the statistics.
+ * \endif
+ * @param majAut :
+ * \if French
+ * booléen qui autorise les majuscules initiales
+ * \else
+ * to neglect the initial uppercase letter.
+ * \endif
  * @return
+ * \if French
+ * le texte scandé ou accentué.
+ * \else
+ * the text scanned or accented.
+ * \endif
  *
+ * Cette fonction scande ou accentue le texte donné.
  * Le paramètre accent détermine le comportement de la fonction.
  * S'il vaut 0, le texte est scandé.
  * Non nul, le texte sera accentué et plusieurs options sont possibles :
@@ -316,6 +497,18 @@ QStringList Scandeur::formeq(QString forme, bool *nonTrouve, bool debPhr,
  * Les valeurs >15 sont tronquées à leur quatre bits de poids faibles.
  * Les valeurs 4, 8 et 12 pourraient conduire à des résultats inattendus,
  * bien qu'aujourd'hui elles donnent le même résultat que la valeur 0 (texte scandé).
+ *
+ * Lorsque le texte est scandé (accent = 0) et que le booléen stats est true,
+ * quelques statistiques seront faites sur les schémas métriques des vers
+ * (séquences des longues et des brèves). Une recherche des hexamètres et des
+ * pentamètres sera aussi effectuée, même si le texte est en prose.
+ *
+ * Si un mot est ambigu et que cela conduit à des formes accentuées ou scandées différentes,
+ * elles seront toutes données. La plus probable sera la première
+ * et les autres seront données entre parenthèses.
+ * Pour la scansion, les règles usuelles d'allongement et d'élision
+ * sont appliquées. Les voyelles élidées sont conservées pour que le texte reste
+ * lisible, mais elles sont placées entre crochets droits [ ].
  */
 QString Scandeur::scandeTxt(QString texte, int accent, bool stats, bool majAut)
 {
@@ -424,6 +617,7 @@ QString Scandeur::scandeTxt(QString texte, int accent, bool stats, bool majAut)
         if (stats)
         {
             // Je cherche des vers dans la prose
+//            qDebug() << schemaMetric;
             int ii = 0;
             int numMot = 1;
             int lsch = schemaMetric.count() - 10;
@@ -438,6 +632,7 @@ QString Scandeur::scandeTxt(QString texte, int accent, bool stats, bool majAut)
                 result.clear();
                 if (ii < lsch && schemaMetric[ii] != '-')
                     result = cherchePieds(6, schemaMetric, ii, false);
+                // L'hexamètre et le pentamètre commence avec une longue ou une commune.
                 // analyse du résultat
                 QString numero;
                 numero.setNum(ii);
@@ -446,6 +641,7 @@ QString Scandeur::scandeTxt(QString texte, int accent, bool stats, bool majAut)
                 {
                     if (item.count() == 6)
                     {
+                        // Hexamètres (eg DSDSDX) et pentamètres (eg DSYSSZ) ont 6 lettres.
                         if (ajout == "")
                             ajout = "<span style='color:red' title='" + item;
                         else
@@ -461,21 +657,35 @@ QString Scandeur::scandeTxt(QString texte, int accent, bool stats, bool majAut)
                                 item[a] == 'Z')
                                 syllabes += 1;
                         }
+//                        qDebug() << ii << item << decalage << numMot << syllabes << schemaMetric.mid(ii, 2*syllabes);
                         int j = ii;
-                        int nbMots = 1;
+                        int nbMots = 1; // ToDo Vérifier ce qui se passe si la dernière syllabe est élidée :
+                        // dSsSDX : mūltă (mūltā) quŏqu[e] (quōqu[e] quō̆qu[e]) ēt bēllō (bēllō̆) pāssūs, dūm cōndĕrĕt ūrb[em], īnfērrētquĕ
+                        // L'hexamètre est +* *` + +* ++ + +-- +`, l'élision finale ne se faisant pas avec le vers suivant.
                         while (syllabes > 0)
                         {
                             if (schemaMetric[j] == '?' ||
                                 schemaMetric[j] == '@')
-                                j += 1;
+                            {
+                                while (schemaMetric[j] != ' ')
+                                    j += 1;
+                                // mot inconnu ou au nombre de syllabes indéterminé (eg Troiae)
+                                // Je le saute (même si c'est improbable...)
+                                nbMots += 2;
+                            }
                             else if (longueurs.contains(schemaMetric[j]))
                             {
                                 j += 1;
                                 syllabes -= 1;
                             }
+                            else if ((schemaMetric[j] == '`') && (syllabes == 1))
+                            {
+                                // J'ai une dernière syllabe qui est élidée à tort.
+                                syllabes = 0;
+                            }
                             else
                             {
-                                nbMots += 2;
+                                nbMots += 2; // mot et séparateur.
                                 while (!longueurs.contains(schemaMetric[j]) &&
                                        (j < schemaMetric.size()))
                                     j += 1;
@@ -504,6 +714,8 @@ QString Scandeur::scandeTxt(QString texte, int accent, bool stats, bool majAut)
                 while ((schemaMetric[ii] != ' ') && ii < lsch) ii += 1;
                 numMot += 2;
                 // Je suis sur le blanc qui précède un mot
+                if (schemaMetric[ii+1] == '@' || schemaMetric[ii+1] == '?')
+                    numMot +=2; // J'ai un mot inconnu ou ambigu.
             }
             // Je remplace les +-* par des signes plus conventionnels
             schemaMetric.replace('-', "∪");
